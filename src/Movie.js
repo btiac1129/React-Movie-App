d75d3f4063e31be0a68a39cf068447df82ae44c5
btiac1129 <btiac@DESKTOP-LC7U1F9.localdomain>
2020-05-23 12:49:53 +0900

    give movieImages data to MoviePoster - third level Component

--- src/Movie.js
+++ src/Movie.js
@@ -5,7 +5,7 @@ class Movie extends Component {
     render() {
         return (
             <div>
-                <MoviePoster />
+                <MoviePoster poster={this.props.poster} />
                 <h1>{this.props.title}</h1>
             </div>
         );
@@ -15,7 +15,7 @@ class Movie extends Component {
 class MoviePoster extends Component {
     render() {
         return (
-            <img src="https://api.time.com/wp-content/uploads/2014/11/hunger-games.jpg?w=800&quality=85" />
+            <img src={this.props.poster} />
         );
     }
 }
