4521ede4a42eb99971d3d8bf2d5dfb58dc82f688
btiac1129 <btiac@DESKTOP-LC7U1F9.localdomain>
2020-05-23 14:36:43 +0900

    create Movies list

--- src/App.js
+++ src/App.js
@@ -16,6 +16,26 @@ const movieImages = [
     "https://lumiere-a.akamaihd.net/v1/images/star-wars-the-rise-of-skywalker-theatrical-poster-1000_ebc74357.jpeg?region=0%2C0%2C891%2C1372"
 ]
 
+const Movies = [
+    {
+        title: "Matrix",
+        poster: "https://img1.looper.com/img/gallery/movies-you-should-watch-if-you-like-the-matrix/intro-1585252166.jpg"
+    },
+    {
+        title: "Full Metal Jacket",
+        poster: "https://resizing.flixster.com/IHsuuyne87RTC_G_StnIEL5KVng=/206x305/v1.bTsxMTE2ODAyOTtqOzE4NDg5OzEyMDA7ODAwOzEyMDA"
+
+    },
+    {
+        title: "Oldboy",
+        poster: "https://lh3.googleusercontent.com/proxy/ghpKgEjT8hIcJOjYIs0BVgk7PUvv8qKHP6_ZE5y1dsymd1E4nW53MjRw4tdEb9cqoyDvEuNvdwqE-_ZA_GM3ZmkQVrZVcPuDQ7V-FYDfwnX_FWSPePQU_W-NsnIyrznIn5rSSrNUalfuXj6gjWDtasKa10ANpZyBYEpt9aw"
+    },
+    {
+        title: "Star Wars",
+        poster: "https://lumiere-a.akamaihd.net/v1/images/star-wars-the-rise-of-skywalker-theatrical-poster-1000_ebc74357.jpeg?region=0%2C0%2C891%2C1372"
+    }
+]
+
 class App extends Component {
     render() {
         return (
