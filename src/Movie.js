3a25f173a7d53aeae933dbe55ed58b124d21e6dc
btiac1129 <btiac1129@gmail.com>
2020-05-30 11:33:25 +0900

    change Movie Component : class -> func

--- src/Movie.js
+++ src/Movie.js
@@ -2,28 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css'
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
+function Movie({ title, poster }) {
+    return (
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    );
 }
 
-//same but, dumb component
+//same but, dumb component - because don't have 'state', so stateless.
 function MoviePoster({ poster }) {
     return (
         <img src={poster} alt="Movie Poster" />
     )
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+
+// how check prop types of the component?
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
