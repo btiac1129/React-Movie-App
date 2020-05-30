e7ee12d6c008fc9da49af88329d393249ece4fb8
soseoso <soseoso@naver.com>
2020-05-30 10:51:18 +0900

    setState -> Loading States

--- src/App.js
+++ src/App.js
@@ -2,36 +2,45 @@ import React, { Component } from "react";
 import "./App.css";
 import Movie from './Movie';
 
-const movies = [
-    {
-        title: "Matrix",
-        poster: "https://img1.looper.com/img/gallery/movies-you-should-watch-if-you-like-the-matrix/intro-1585252166.jpg"
-    },
-    {
-        title: "Full Metal Jacket",
-        poster: "https://resizing.flixster.com/IHsuuyne87RTC_G_StnIEL5KVng=/206x305/v1.bTsxMTE2ODAyOTtqOzE4NDg5OzEyMDA7ODAwOzEyMDA"
-
-    },
-    {
-        title: "Oldboy",
-        poster: "https://lh3.googleusercontent.com/proxy/ghpKgEjT8hIcJOjYIs0BVgk7PUvv8qKHP6_ZE5y1dsymd1E4nW53MjRw4tdEb9cqoyDvEuNvdwqE-_ZA_GM3ZmkQVrZVcPuDQ7V-FYDfwnX_FWSPePQU_W-NsnIyrznIn5rSSrNUalfuXj6gjWDtasKa10ANpZyBYEpt9aw"
-    },
-    {
-        title: "Star Wars",
-        poster: "https://lumiere-a.akamaihd.net/v1/images/star-wars-the-rise-of-skywalker-theatrical-poster-1000_ebc74357.jpeg?region=0%2C0%2C891%2C1372"
-    }
-]
-
 class App extends Component {
+
+    state = {
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                movies: [{
+                    title: "Matrix",
+                    poster: "https://img1.looper.com/img/gallery/movies-you-should-watch-if-you-like-the-matrix/intro-1585252166.jpg"
+                },
+                {
+                    title: "Full Metal Jacket",
+                    poster: "https://resizing.flixster.com/IHsuuyne87RTC_G_StnIEL5KVng=/206x305/v1.bTsxMTE2ODAyOTtqOzE4NDg5OzEyMDA7ODAwOzEyMDA"
+
+                },
+                {
+                    title: "Oldboy",
+                    poster: "https://lh3.googleusercontent.com/proxy/ghpKgEjT8hIcJOjYIs0BVgk7PUvv8qKHP6_ZE5y1dsymd1E4nW53MjRw4tdEb9cqoyDvEuNvdwqE-_ZA_GM3ZmkQVrZVcPuDQ7V-FYDfwnX_FWSPePQU_W-NsnIyrznIn5rSSrNUalfuXj6gjWDtasKa10ANpZyBYEpt9aw"
+                },
+                {
+                    title: "Star Wars",
+                    poster: "https://lumiere-a.akamaihd.net/v1/images/star-wars-the-rise-of-skywalker-theatrical-poster-1000_ebc74357.jpeg?region=0%2C0%2C891%2C1372"
+                }]
+            })
+        }, 7000)
+    }
+
     render() {
         return (
-            <div className="App">
-                {movies.map(movie => {
-                    return <Movie title={movie.title} poster={movie.poster} />
-                })}
+            <div className="App" >
+                {
+                }
             </div>
         );
     }
+
+
 }
 
 export default App;
