fde1d5821483d46ff90611a23ed756d428bc9320
btiac1129 <btiac1129@gmail.com>
2020-05-30 13:21:53 +0900

    await, async

--- src/App.js
+++ src/App.js
@@ -6,20 +6,30 @@ class App extends Component {
     state = {}
 
     componentDidMount() {
-        fetch("https://yts.mx/api/v2/list_movies.json?quality=3D/sort_by=rating")
-            .then(response => response.json())
-            .then(json => console.log(json))
-            .catch(err => console.log(err))
-
+        this._getMovies();
     }
 
 
 
     _renderMovies = () => {
         const movies = this.state.movies.map((movie, index) => {
-            return <Movie title={movie.title} poster={movie.poster} key={index} />
+            return <Movie title={movie.title} poster={movie.large_cover_image} key={index} />
         })
         return movies
+    }
+
+    _getMovies = async () => {
+        const movies = await this._callApi()
+        this.setState({
+            movies
+        })
+    }
+
+    _callApi = () => {
+        return fetch("https://yts.mx/api/v2/list_movies.json?quality=3D/sort_by=rating")
+            .then(response => response.json())
+            .then(json => json.data.movies)
+            .catch(err => console.log(err))
     }
 
     render() {
